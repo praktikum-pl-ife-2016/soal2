363227e0210ffe0d1a3e1ebe7483d32d77b4c3cc
TerryKincowati <terrykincowati.tk@gmail.com>
2016-03-07 17:43:01 +0700

    sent soal2

---
+++ src/Soal1.java
@@ -0,0 +1,40 @@
+package SOAL;
+public class Soal1 {   
+    private String namaMahasiswa;
+    private int noAbsen;
+    private String nim;
+    private int usia;
+    private String mataKuliah;
+    
+    public void setNamaMahasiswa(String s) {
+        namaMahasiswa = s;
+    }
+    
+    public void setNoAbsen(int i) {
+        noAbsen = i;
+    }
+
+    public void setNIM(String s) {
+        nim = s;
+    }
+
+    public void setUsia(int i) {
+        usia = i;
+    }
+
+    public void setMataKuliah(String s) {
+        mataKuliah = s;
+    }
+
+    public void displayMessage() {
+        System.out.println("Nama\t\t: " +namaMahasiswa);
+        System.out.println("Nim\t\t: " +nim);
+        System.out.println("No Absen\t: " +noAbsen);
+        System.out.println("Usia\t\t: "+usia);
+        System.out.println("Mata kuliah\t: " +mataKuliah);
+    }
+    
+    //Tidak usah dikasih main method
+    //main method hanya pada mainClass
+    
+    }
