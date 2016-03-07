7cb2d0b40cee2d67a3c2345d45f35c35ccbec041
nindasilviatricahyani@gmail.com <iwisuharti@gmail.com>
2016-03-07 22:47:37 +0700

    Source code

---
+++ src/MainKandidat1.java
@@ -0,0 +1,18 @@
+
+
+public class MainKandidat1 {
+    public static void main(String[] args) {
+Kandidat1 kandidat1 = new Kandidat1();
+kandidat1.setNama("Ninda");
+kandidat1.setAttitude(90);
+kandidat1.setPenampilan(90);
+kandidat1.setSuara(75);
+Kandidat1 kandidat2 = new Kandidat1();
+kandidat2.setNama("Silvia");
+kandidat2.setAttitude(80);
+kandidat2.setPenampilan(75);
+kandidat2.setSuara(90);
+kandidat1.Compare(kandidat2);
+}
+}
+
