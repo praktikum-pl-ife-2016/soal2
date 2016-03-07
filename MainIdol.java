749227b58b56a6f7f6e1d713fcb0c9208eb89dbc
andika22 <andikaisranugraha97@gmail.com>
2016-03-07 20:43:10 +0700

    Update MainIdol.java

--- MainIdol.java
+++ MainIdol.java
@@ -5,16 +5,16 @@ public class MainIdol {
 public static void main(String[] args) {
         
 Idol m1 = new Idol ();
-m1.setNama ("Hardwell");
-m1.setAttitude (90);
-m1.setPenampilan (89);
-m1.setSuara (79);
+m1.setNama ("X-Silent");
+m1.setAttitude (78);
+m1.setPenampilan (86);
+m1.setSuara (72);
 
 Idol m2 = new Idol ();
-m2.setNama ("David Guetta");
-m2.setAttitude (50);
-m2.setPenampilan (87);
-m2.setSuara (98);
-Idol.banding(m1,m2);
+m2.setNama ("Rhomzie");
+m2.setAttitude (60);
+m2.setPenampilan (82);
+m2.setSuara (91);
+Idol.comparison(m1,m2);
     }
 }
