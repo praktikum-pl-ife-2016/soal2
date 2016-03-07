c71fea0dc7c12950b177252f86bfd3e622d625e4
rizqifauzi <rizqif.axe@gmail.com>
2016-03-07 22:58:44 +0700

    PerbandinganPeserta.java added

---
+++ src/PerbandinganPeserta.java
@@ -0,0 +1,9 @@
+public class PerbandinganPeserta {
+
+    public static void main(String[] args) {
+    
+        Peserta peserta1 = new Peserta();
+        Peserta peserta2 = new Peserta();
+        peserta1.Perbandingan(peserta2);
+    }
+}
