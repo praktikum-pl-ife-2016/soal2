8c4c8f1d02520287f146e36dd203acca5981752f
imamutomo3 <0310imam@gmail.com>
2016-03-07 19:20:11 +0700

    Source Code

---
+++ MainKandidat.java
@@ -0,0 +1,11 @@
+package mainkandidat;
+/**
+ *
+ * @author Imam
+ */
+public class MainKandidat {
+    public static void main(String[] args) {
+        Kandidat hasil = new Kandidat();
+     hasil.bandingkan(hasil);
+    }    
+}
