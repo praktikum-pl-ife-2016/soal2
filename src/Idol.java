7f12d098966132bb4bdc1a8cd26978fade008b82
KelvinAP <kelvinaditya87@gmail.com>
2016-03-07 22:12:35 +0700

    Pengumpulan Soal 2

---
+++ src/Idol.java
@@ -0,0 +1,37 @@
+
+
+class Idol {
+
+    private String nama1;
+    private int poinA;
+    private int pointAt;
+    private int poinS;
+
+    public Idol() {
+
+    }
+
+    public Idol(String nama1, int poinA, int pointAt, int poinS) {
+        this.nama1 = nama1;
+        this.poinA = poinA;
+        this.pointAt = pointAt;
+        this.poinS = poinS;
+    }
+
+    public String getNama1() {
+        return nama1;
+    }
+
+    public int getPoinA() {
+        return poinA;
+    }
+
+    public int getPointAt() {
+        return pointAt;
+    }
+
+    public int getPoinS() {
+        return poinS;
+    }
+
+}
