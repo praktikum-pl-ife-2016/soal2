ac745cafc48b98b43615f5462fb9b8057bc437f5
rasioatmaja29 <rasioatmaja29@gmail.com>
2016-03-07 16:43:20 +0700

    Update1

---
+++ src/MainKandidat.java
@@ -0,0 +1,18 @@
+public class MainKandidat {
+    public static void main(String[] args) {
+       Kandidat k1 = new Kandidat ();
+          k1.setNama ("Hardwell");
+          k1.setAttidute (90);
+          k1.setPenampilan (89);
+          k1.setSuara (79);
+       Kandidat k2 = new Kandidat ();
+          k2.setNama ("David Guetta");
+          k2.setAttidute (50);
+          k2.setPenampilan (87);
+          k2.setSuara (98);
+          k1.banding(k2);
+    }
+    
+}
+
+
