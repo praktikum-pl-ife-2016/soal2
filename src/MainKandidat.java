2d8272235e84bf9ae257019065ec1cc2a2216847
luthfipratama <luthfip20.lp@gmail.com>
2016-03-07 17:53:35 +0700

    upload MainKandidat.java

--- src/MainKandidat.java
+++ src/MainKandidat.java
@@ -1,6 +1,7 @@
 public class MainKandidat {
 
-    public static void main(String[] args) {
+    
+	public static void main(String[] args) {
     Kandidat nama1 =  new Kandidat(); //instan objek bernama nama1
     Kandidat nama2 =  new Kandidat(); //instan objek bernama nama2
     nama1.bandingkan(nama2);// memanggil method bandingkan dengan membandingkan nama1 dan nama2
